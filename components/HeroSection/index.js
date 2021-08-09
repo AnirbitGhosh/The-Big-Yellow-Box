6cf43b41876126c1864dac974b733ddedcf9f5e2
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-08-09 21:34:58 +0530

    added &apos support

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -36,7 +36,7 @@ const HeroSection = (props) => {
                 {/* <VideoBg autoPlay={true} loop muted src={Video} type="video/mp4" /> */}
             </HeroBg>
             <HeroContent>
-                <HeroH1>Type your business idea, thoughts, feelings, confessions or what you had for breakfast this morning into the big yellow box. If we like your idea, we'll go with it and give you back 10% of the profits. That's it!</HeroH1>
+                <HeroH1>Type your business idea, thoughts, feelings, confessions or what you had for breakfast this morning into the big yellow box. If we like your idea, we&apos;ll go with it and give you back 10% of the profits. That&apos;s it!</HeroH1>
                 <br />
                 <FormWrap>
                     <FormContent>
